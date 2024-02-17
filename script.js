20753402a05eab290dc0796c32bebcf9241f53f7
ibrahimGoumrane <152559100+ibrahimGoumrane@users.noreply.github.com>
2024-02-17 22:07:26 +0100

    using the intersection observer in ordre to animate my site

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ const navigation = document.querySelector('.header .navigation');
 const lis = document.querySelectorAll('.header .navigation li');
 if (window.innerWidth < 992) {
     icone_header.addEventListener('click', function () {
-        console.log(12);
         navigation.classList.toggle('initial');
     })
     lis.forEach((li) => {
@@ -26,3 +25,40 @@ if (window.innerWidth < 992) {
 else {
     navigation.classList.remove('initial');
 }
+
+
+const sections = document.querySelectorAll('section');
+const h1_projects = document.querySelectorAll('.projects h3');
+const projects_container = document.querySelectorAll('.projects .projects_container')
+let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.4,
+};
+
+const sectionObserver = new IntersectionObserver(function (entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('show');
+    if (entry.target.classList.contains('projects')) {
+        console.log(entry.target);
+        h1_projects.forEach((h3, index) => {
+            const timeH1 = setTimeout(() => {
+                h3.style.transform = 'translateX(0px)';
+                clearTimeout(timeH1);
+            }, index * 600)
+        });
+        projects_container.forEach((project, index) => {
+            const timeContainer = setTimeout(() => {
+                project.style.transform = 'translateX(0px)';
+                clearTimeout(timeContainer);
+            }, index * 600)
+        });
+    }
+    observer.unobserve(entry.target);
+}, options)
+sections.forEach((section) => {
+    sectionObserver.observe(section);
+})
+
